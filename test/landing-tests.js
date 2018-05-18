3b001f9b43ec53fdafea6e211e3129f89b3c6a3a
mcannon <m.cannon.dev@gmail.com>
2018-05-18 15:36:30 +0100

    Should have a simple test running in Chrome headless via Puppeteer.

--- test/landing-tests.js
+++ test/landing-tests.js
@@ -1,8 +1,27 @@
+import "babel-polyfill";
+import * as Chai from "chai";
+const Puppeteer = require("puppeteer");
+
+Chai.should();
+
 describe("Landing", function() {
     describe("Content", function() {
-        it("should be 'X'", function() {
-            let content = "";
-            content.should.be("X");
+        it("Title should be 'MVP'", function(done) {
+            var pageTitle;
+            (async () => {
+                const browser = await Puppeteer.launch({ headless: true });
+                const page = await browser.newPage();
+                await page.goto("http://localhost:8080");
+                pageTitle = await page.title();
+                await browser.close();
+            })()
+                .then(x => {
+                    pageTitle.should.equal("MVP");
+                    done(x);
+                })
+                .catch(e => {
+                    done(e);
+                });
         });
     });
 });
